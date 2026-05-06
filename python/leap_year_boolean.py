9cc32d2e2ed152011494895b57512f734e3d392a
madhumitha-s-3327 <madhumitha3327@gmail.com>
2026-05-06 13:41:15 +0530

    leap_year_boolean.py

--- python/leap_year_boolean.py
+++ python/leap_year_boolean.py
@@ -1,11 +1,1 @@
-def is_leap(year):
-    leap = False
-    if (year%4==0):
-        print("True")
-    elif (year%400==0):
-        print("True")
-    else:
-        return leap
-    return leap
-year = int(input())
-print(is_leap(year))
+
